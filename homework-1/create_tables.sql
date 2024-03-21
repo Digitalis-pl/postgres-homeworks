0ed566db07d500a912b13116ed572b8a09db0e59
georg <zhora.karsakov@mail.ru>
2024-03-21 19:31:29 +0300

    работа 17.1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar(50) PRIMARY KEY UNIQUE NOT NULL,
+	company_name varchar(70) NOT NULL,
+	contact_name varchar(70) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	emoployee_id int UNIQUE NOT NULL,
+	first_name text NOT NULL,
+	last_name text NOT NULL,
+	title text NOT NULL,
+	birh_date varchar(20) NOT NULL,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int UNIQUE PRIMARY KEY NOT NULL,
+	customer_id varchar(50) NOT NULL
+	REFERENCES customers(customer_id),
+	employee_id int NOT NULL
+	REFERENCES employees(emoployee_id),
+	order_date varchar(20) NOT NULL,
+	ship_city text NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+def read_data(filename):
+    new_list = []
+    with open(f'north_data/{filename}') as file:
+        header = next(file)
+        reader = csv.reader(file)
+        for el in reader:
+            new_list.append(el)
+    return new_list
+
+
+con = psycopg2.connect(
+    host='localhost',
+    database='skysql',
+    user='postgres',
+    password='12345')
+
+try:
+    with con:
+        with con.cursor() as cur:
+            for el1 in read_data('customers_data.csv'):
+                cur.execute('INSERT INTO customers VALUES(%s, %s, %s)', el1)
+            for el2 in read_data('employees_data.csv'):
+                cur.execute('INSERT INTO employees VALUES(%s, %s, %s, %s, %s, %s)', el2)
+            for el3 in read_data('orders_data.csv'):
+                cur.execute('INSERT INTO orders VALUES(%s, %s, %s, %s, %s)', el3)
+finally:
+    con.close()
